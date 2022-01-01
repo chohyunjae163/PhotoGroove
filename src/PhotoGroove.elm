13c261c8546309a41eb6b36fdc84a6473a2a278b
chohyunjae <chohyunjae163@gmail.com>
2022-01-01 22:47:20 +0900

    update model view

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -2,21 +2,63 @@ module PhotoGroove exposing (main)
 
 import Html exposing (div, h1,img, text)
 import Html.Attributes exposing(..)
+import Html.Events exposing (onClick)
+import Browser
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
 
 
-view : a -> Html.Html msg
+--tranlates the current model into a desired DOM structure
+view :  { photos : List { url : String }, selectedUrl : String} ->  Html.Html { description : String, data : String }
 view model =
   div [class "content" ]
       [ h1 [] [text "Photo Groove"]
-      , div [ id "thumbnails" ]
-          [ img [ src "http://elm-in-action.com/1.jpeg"] []
-          , img [ src "http://elm-in-action.com/2.jpeg"] []
-          , img [ src "http://elm-in-action.com/3.jpeg"] []
-          ]
+      , div [ id "thumbnails" ] 
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
       ]
 
 
+--viewThumbnail : String -> { url : String } -> Html.Html msg
+viewThumbnail : String -> { url : String } -> Html.Html { description : String, data : String }
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick { description = "ClickedPhoto", data = thumb.url }
+        ]
+         []
 
-main : Html.Html msg
+
+initialModel : { photos : List { url : String }, selectedUrl : String}
+initialModel =
+    { photos = 
+        [ {url = "1.jpeg"}
+        , {url = "2.jpeg"}
+        , {url = "3.jpeg"}
+        ]
+    , selectedUrl = "1.jpeg"    
+    }
+
+
+--update : {  description : String, data : b } -> { selectedUrl : b } -> { selectedUrl : b }
+update msg model =
+    if msg.description == "ClickedPhoto" then
+        { model | selectedUrl = msg.data}
+
+    else
+        model
+
+--main : Html.Html msg
+main : Program () { photos : List { url : String }, selectedUrl : String } { description : String, data : String }
 main =
-    view "no model yet"
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
