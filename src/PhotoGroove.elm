e3cd3a62c6935a576e96701211e0f21c4e3b93c8
chohyunjae <chohyunjae163@gmail.com>
2022-01-02 18:26:05 +0900

    Part 1 is finished!

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,20 +1,42 @@
 module PhotoGroove exposing (main)
 
-import Html exposing (div, h1,img, text)
+import Html exposing (..)
 import Html.Attributes exposing(..)
 import Html.Events exposing (onClick)
 import Browser
+import Array exposing (Array)
+import Random
+
 urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
 
+
+type Msg
+    = ClickedPhoto String
+    | GotSelectedIndex Int
+    | ClickedSize ThumbnailSize
+    | ClickedSurpriseMe
+
+
+
+
+
+
+
 --tranlates the current model into a desired DOM structure
-view :  { photos : List { url : String }, selectedUrl : String} ->  Html.Html { description : String, data : String }
+view : Model -> Html Msg
 view model =
   div [class "content" ]
       [ h1 [] [text "Photo Groove"]
-      , div [ id "thumbnails" ] 
+      , button
+            [ onClick ClickedSurpriseMe ]
+            [ text "Surprise Me!"]
+      , h3 [] [text "thumbnail Size: "]
+      , div [ id "choose-size"]
+            (List.map viewSizeChooser [ Small,Medium,Large ] )
+      , div [ id "thumbnails", class (sizeToString model.chosenSize) ] 
             (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img
             [ class "large"
@@ -24,41 +46,99 @@ view model =
       ]
 
 
---viewThumbnail : String -> { url : String } -> Html.Html msg
-viewThumbnail : String -> { url : String } -> Html.Html { description : String, data : String }
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
     img
         [ src (urlPrefix ++ thumb.url)
         , classList [ ( "selected", selectedUrl == thumb.url ) ]
-        , onClick { description = "ClickedPhoto", data = thumb.url }
+        , onClick (ClickedPhoto thumb.url)
         ]
          []
 
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input [ type_ "radio", name "size", onClick (ClickedSize size)] []
+        , text (sizeToString size)
+        ]
+    
 
-initialModel : { photos : List { url : String }, selectedUrl : String}
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
+
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+type alias Photo =
+    { url : String }
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    , chosenSize : ThumbnailSize
+    }
+
+initialModel : Model
 initialModel =
     { photos = 
         [ {url = "1.jpeg"}
         , {url = "2.jpeg"}
         , {url = "3.jpeg"}
         ]
-    , selectedUrl = "1.jpeg"    
+    , selectedUrl = "1.jpeg"
+    , chosenSize = Small    
     }
 
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
 
---update : {  description : String, data : b } -> { selectedUrl : b } -> { selectedUrl : b }
+getPhotoUrl : Int -> String
+getPhotoUrl index =
+    case Array.get index photoArray of
+        Just photo ->
+            photo.url
+
+        Nothing ->
+            ""
+
+randomPhotoPicker : Random.Generator Int
+randomPhotoPicker =
+    Random.int 0 (Array.length photoArray - 1)
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    if msg.description == "ClickedPhoto" then
-        { model | selectedUrl = msg.data}
+    case msg of
+        GotSelectedIndex index ->
+            ({model | selectedUrl = getPhotoUrl index}, Cmd.none)
 
-    else
-        model
+        ClickedPhoto url ->
+            ( { model | selectedUrl = url }, Cmd.none)
 
---main : Html.Html msg
-main : Program () { photos : List { url : String }, selectedUrl : String } { description : String, data : String }
+        ClickedSize size ->
+            ( { model | chosenSize = size },Cmd.none)
+
+        ClickedSurpriseMe ->
+            ( model, Random.generate GotSelectedIndex randomPhotoPicker )
+
+        
+main : Program() Model Msg
 main =
-    Browser.sandbox
-        { init = initialModel
+    Browser.element
+        { init = \() -> ( initialModel, Cmd.none)
         , view = view
         , update = update
+        , subscriptions = \model -> Sub.none
         }
