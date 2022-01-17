00d817408d81578735a629b022ce62783e23cc5c
chohyunjae <chohyunjae163@gmail.com>
2022-01-17 23:07:34 +0900

    elm tests

---
+++ tests/PhotoGrooveTests.elm
@@ -0,0 +1,23 @@
+module PhotoGrooveTests exposing (..)
+
+
+import Json.Encode as Encode
+import Json.Decode as Decode
+import PhotoGroove
+import Test exposing (..)
+import Expect
+import Fuzz exposing (Fuzzer, int, list, string)
+
+
+decoderTest : Test
+decoderTest =
+    fuzz2 string int "title defaults to (untitled)" <|
+        \url size ->
+            [ ( "url", Encode.string url)
+            , ( "size", Encode.int size)
+            ]
+                |> Encode.object
+                |> Decode.decodeValue PhotoGroove.photoDecoder
+                |> Result.map .title
+                |> Expect.equal (Ok "(untitled)")
+        
