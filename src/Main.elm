6161b1b59c13a02bf4792ba7a531b9cb672b6d98
chohyunjae <chohyunjae163@gmail.com>
2022-01-24 23:35:42 +0900

    page 263. single-page application

--- src/Main.elm
+++ src/Main.elm
@@ -4,15 +4,60 @@ import Browser exposing (Document)
 import Html exposing (Html, a, footer, h1, li, nav, text, ul)
 import Html.Attributes exposing (classList,href)
 import PhotoFolders exposing (Msg)
+import Html.Lazy exposing (lazy)
+import Browser.Navigation as Nav
+import Url exposing (Url)
+
 
 type alias Model =
-    {}
+    { page : Page }
+
+type Page
+    = Gallery
+    | Folders
+    | NotFound
+
 
 view : Model -> Document Msg
 view model =
+    let
+        content =
+            text " This isn't even my final form!"
+    in
     { title = "Photo Groove, SPA Style"
-    , body = [ text " This isn't even my final form!" ]
+    , body =
+        [ lazy viewHeader model.page
+        , content
+        , viewFooter
+        ]
     }
+
+viewHeader : Page -> Html Msg
+viewHeader page =
+    let
+        logo =
+            h1 [] [ text "Photo Groove"]
+
+        links =
+            ul []
+                [ navLink Folders { url = "/", caption = "Folders"}
+                , navLink Gallery { url = "/gallery", caption = "Gallery"}
+                ]
+
+        navLink : Page -> { url : String, caption : String } -> Html msg
+        navLink targetPage { url, caption } =
+            li [ classList [ ( "active", page == targetPage) ] ]
+                [ a [ href url ] [ text caption ] ]
+    in
+    nav [] [ logo, links ]
+
+
+viewFooter : Html msg
+viewFooter =
+    footer [] [ text "One is never alone with a rubber duck. - Douglas Adams"
+    ]
+
+
 
 type Msg
     = NothingYet
@@ -29,9 +74,24 @@ subscriptions model =
 
 main : Program () Model Msg
 main =
-    Browser.document
-        { init = \_ -> ({}, Cmd.none)
+    Browser.application
+        { init = \_ _ _ -> ( { page = Folders }, Cmd.none)
+        , onUrlRequest = \_ -> Debug.todo "handle URL requests"
+        , onUrlChange = \_ -> Debug.todo "handle URL changes"
         , view = view
         , update = update
         , subscriptions = subscriptions
         }
+
+
+init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    case url.path of
+        "/gallery" ->
+            ( { page = Gallery }, Cmd.none )
+
+        "/" ->
+            ( { page = Folders }, Cmd.none )
+
+        _ ->
+            ( { page = NotFound }, Cmd.none )
